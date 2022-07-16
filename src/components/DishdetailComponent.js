dd50fe52e26dadb73f45c885da141bd3f633f49d
squall4-lol <sergio_bandera@hotmail.com>
2022-07-16 15:05:49 +0200

    task 1 completada

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,27 +1,57 @@
 import React, { Component } from "react";
+import {
+    Card,
+    CardImg,
+    CardImgOverlay,
+    CardText,
+    CardBody,
+    CardTitle,
+  } from "reactstrap";
 
 class DishDetail extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+    renderDish(dish) {
+    if (dish != null)
+      return (
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    else return <div></div>;
+  }
 
-        this.state = {
-            dishes:null
-        }
-    }
 
 
 
+  render() {
+    const propDish = this.props.dish;
+    
+    // const propDish = this.props.dish.map ((dish) => {
+    //     <>
+    //     <p>vale</p>
+    //     </>
 
-    render() {
-        const selectedDish = this.props.dishes;
-        return (
+    // })
 
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    {/* {this.renderDish(this.state.selectedDish)} */}
-                </div>
-            </div>
-        );
-    }
+
+    return (
+        <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          {this.renderDish(this.props.dish)}
+        </div>
+        <div>
+            {propDish ? 
+            <Card><p>{propDish.name}</p></Card> : <div></div>
+            }
+        </div>
+      </div>
+    );
+  }
 }
 export default DishDetail;
